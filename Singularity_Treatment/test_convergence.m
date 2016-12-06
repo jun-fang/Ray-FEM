f34cb22f1cf780ecc3fc6a84a32fce886555d500
dinrker <junf2@uci.edu>
2016-12-06 02:38:19 -0800

    RayFEM: omega convergence, finally figured out the reason, need to use high order quadrature rule to compute the L^2 error

--- Singularity_Treatment/test_convergence.m
+++ Singularity_Treatment/test_convergence.m
@@ -111,7 +111,7 @@ if (0)
     end
     
     
-    figure(1);
+    figure(12);
     subplot(1,2,1);
     show_convergence_rate(omegas,rhss,'omega',[],'||f||_{L^2(\Omega)}');
     subplot(1,2,2);
@@ -125,7 +125,7 @@ end
 
 
 %% Ray-FEM h convergence:  error \sim O(h^2)
-if(1)
+if(0)
     xs = 0; ys = 0;
     omega = 40*pi;
     wl = 2*pi/omega;
@@ -154,27 +154,7 @@ if(1)
         ray = atan2(yy,xx);
         ray = exp(1i*ray).*(rr>10*eps);
         
-        A = assemble_Helmholtz_matrix_with_ray_1(node,elem,omega,wpml,sigmaMax,speed,ray,fquadorder);
-        b = assemble_RHS_with_sing_RayFEM(node,elem,xs,ys,omega,epsilon,wpml,sigmaMax,ray,speed,@sing_rhs_homo,fquadorder);
-        
-        %% Boundary conditions
-        [bdNode,~,isBdNode] = findboundary(elem);
-        freeNode = find(~isBdNode);
-        N = size(node,1);  Nray = size(ray,2); n = round(sqrt(N));
-        v = zeros(N,1);
-        v(freeNode) = A(freeNode,freeNode)\b(freeNode);
-        
-        %% construct solution
-        grad = ray(:);
-        grad = [real(grad),imag(grad)];
-        repnode = repmat(node,Nray,1);
-        temp = grad(:,1).*repnode(:,1) + grad(:,2).*repnode(:,2);
-        
-        k = omega./speed(node);           % wavenumber
-        kk = repmat(k,1,Nray);
-        u = v.*exp(1i*kk(:).*temp);
-        u = reshape(u,N,Nray);
-        u = sum(u,2);
+        u = RayFEM_singularity(node,elem,xs,ys,omega,epsilon,wpml,sigmaMax,ray,speed,@sing_rhs_homo,fquadorder);
         
         x = node(:,1); y = node(:,2);
         rr = sqrt((x-xs).^2 + (y-ys).^2);
@@ -188,17 +168,93 @@ if(1)
         idx = find( (x<=max(x)-wpml).*(x>= min(x)+wpml)...
             .*(y<= max(y)-wpml).*(y>= min(y)+wpml) );
         du_phy = du(idx);
-        RFEM_errs(ii) = norm(du_phy)/norm(uex(idx));
+        RFEM_errs(ii) = norm(du_phy)*h;
         toc;
     end
     
-    figure(10);
+    figure(21);
     show_convergence_rate(hs,RFEM_errs,'h',[],'||u - u_h||_{L^2(\Omega)}');
 end
 
 
 
-
+%% Ray FEM omega convergence: error \sim O(\omega^2.5)???
+if (1)
+    xs = 0; ys = 0;
+%     omega = 40*pi;
+    
+    epsilon = sqrt(18/40/pi);
+    speed = @(p) ones(size(p(:,1)));
+    
+    wpml = 0.1;
+    sigmaMax = 25/wpml;
+    fquadorder = 3;
+    a = 1;
+    
+    
+    nt = 3;
+    errors = zeros(1,nt);
+    rhss = zeros(1,nt);
+    omegas = pi*[40,60,80,120,160];
+    NPW = 6;
+    
+    
+    for ii = 1:nt
+        ii
+        omega = omegas(ii);
+        wl = 2*pi/omega;
+        h = 1/round(1/(wl/NPW));
+        1/h
+    
+%     h = 1/800;
+    
+    [node,elem] = squaremesh([-a,a,-a,a],h);
+    
+    %% Exact ray information
+    xx = node(:,1)-xs;  yy = node(:,2)-ys;
+    rr = sqrt(xx.^2 + yy.^2);
+    ray = atan2(yy,xx);
+    ray = exp(1i*ray).*(rr>10*eps);
+    
+    
+    
+%         omega = 40*pi;
+        rhs = sing_rhs_homo(epsilon,omega,node,xs,ys);
+        rhss(ii) = norm(rhs)*h;
+        
+        tic;
+        [u,~,~,v] = RayFEM_singularity(node,elem,xs,ys,omega,epsilon,wpml,sigmaMax,ray,speed,@sing_rhs_homo,fquadorder);
+        
+        x = node(:,1); y = node(:,2);
+        rr = sqrt((x-xs).^2 + (y-ys).^2);
+        
+        ub = 1i/4*besselh(0,1,omega*rr);
+        cf = cutoff(epsilon,2*epsilon,node,xs,ys);
+        uex = (1-cf).*ub;
+        uex(rr<epsilon) = 0;
+        du = u - uex;
+        
+        idx = find( (x<=max(x)-wpml).*(x>= min(x)+wpml)...
+            .*(y<= max(y)-wpml).*(y>= min(y)+wpml) );
+        du_phy = du(idx);
+        
+        dd = 0*du; dd(idx) = du(idx);
+        figure(2);showsolution(node,elem,real(dd));
+        
+        
+        [err, rel_L2_err] = RayFEM_smooth_solution_error(node,elem,xs,ys,omega,epsilon,ray,speed,v,9);
+        errors(ii) = err;%norm(du_phy)*h;%/norm(uex(idx));
+        toc;
+    end
+    
+    
+    figure(22);
+    subplot(1,2,1);
+    show_convergence_rate(omegas(1:nt),rhss,'omega',[],'||f||_{L^2(\Omega)}');
+    subplot(1,2,2);
+    show_convergence_rate(omegas(1:nt),errors,'omega',[],'||u - u_h||_{L^2(\Omega)}');
+    
+end
 
 
 
