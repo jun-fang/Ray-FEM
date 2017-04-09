1ee838da2a63b53f74a50dc7b02b7eee2a54b6b1
dinrker <junf2@uci.edu>
2017-04-09 12:59:17 -0800

    tune parameters of NMLA: M, Rest and h_c

---
+++ Singularity_Treatment/Far_Field/test.m
@@ -0,0 +1,14 @@
+clear;
+NPW = 4;                   % number of points per wavelength
+test_num = 8;              % we test test_num examples
+test1_hom_num_ray_annulus_mesh(NPW, test_num);
+
+clear;
+NPW = 6;                   % number of points per wavelength
+test_num = 7;              % we test test_num examples
+test1_hom_num_ray_annulus_mesh(NPW, test_num);
+
+clear;
+NPW = 8;                   % number of points per wavelength
+test_num = 6;              % we test test_num examples
+test1_hom_num_ray_annulus_mesh(NPW, test_num);
