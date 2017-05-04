0ef34d06cde4f3ebdc5d96440adb753f108990bb
dinrker <junf2@uci.edu>
2017-05-04 12:30:57 -0800

    update

--- NMLA/interpolation2.m
+++ NMLA/interpolation2.m
@@ -4,11 +4,16 @@ function uint = interpolation2(x, y, u, xy)
 
 xmin = min(x);    xmax = max(x);
 ymin = min(y);    ymax = max(y);
-if min(xy(:,1))<xmin-eps || max(xy(:,1))>xmax+eps...
-        || min(xy(:,2))<ymin-eps || max(xy(:,2))>ymax+eps
-    fprintf('Attention!!! xy nodes out of the interpolation domain !!!\n');
-    return;
-end
+% if min(xy(:,1))<xmin-eps || max(xy(:,1))>xmax+eps...
+%         || min(xy(:,2))<ymin-eps || max(xy(:,2))>ymax+eps
+%     fprintf('Attention!!! xy nodes out of the interpolation domain !!!\n');
+%     return;
+% end
+
+
+cu = size(u,2);              % column number of u
+rxy = size(xy,1);            % row number of xy
+uint = zeros(rxy,cu);        % output
 
 n = length(x);
 h = (xmax - xmin) / (n - 1);
@@ -16,10 +21,13 @@ h = (xmax - xmin) / (n - 1);
 ix = 1 + floor((xy(:,1)-xmin)/h + eps);     % find the location in x coordinate
 iy = 1 + floor((xy(:,2)-ymin)/h + eps);     % find the location in y coordinate
 
+ix = min(ix, n-1);  ix = max(ix, 1);
+iy = min(iy, n-1);  iy = max(iy, 1);
+
 % 4--3
 %  | \  |
-% 1--2   
-
+% 1--2
+x = x(:);  y = y(:);
 xy1 = [x(ix), y(iy)];
 xy2 = [x(ix+1), y(iy)];
 xy3 = [x(ix+1), y(iy+1)];
@@ -34,9 +42,20 @@ a34 = tri_area(xy3, xy4, xy)/area;
 a41 = tri_area(xy4, xy1, xy)/area;
 
 % interpolation
-uint1 = a12.*u(iy+1, ix) + a24.*u(iy, ix) + a41.*u(iy, ix+1);
-uint2 = a23.*u(iy+1, ix) + a24.*u(iy+1, ix+1) + a34.*u(iy, ix+1);
-uint = uint1.*( sum(xy,2) <= sum(xy1)+h) + uint2.*( sum(xy,2) > sum(xy1)+h) ;
-
+for i = 1: cu
+    uint1 = a12.*findu(ix, iy+1, n, u(:,i)) + a24.*findu(ix, iy, n, u(:,i)) + a41.*findu(ix+1, iy, n, u(:,i)) ;
+    uint2 = a23.*findu(ix, iy+1, n, u(:,i)) + a24.*findu(ix+1, iy+1, n, u(:,i)) + a34.*findu(ix+1, iy, n, u(:,i)) ;
+    uint(:,i) = uint1.*( sum(xy,2) <= sum(xy1,2)+h) + uint2.*( sum(xy,2) > sum(xy1,2)+h) ;
+end
 
 end
+
+
+function uxy = findu(ix, iy, n, u)
+ind = (ix - 1)*n + iy;
+uxy = u(ind,:);
+end
+
+
+
+
---
+++ Plots_Prints/python_scripts/paper2/ex2_Lippmann_Schwinger.py
@@ -0,0 +1,94 @@
+import numpy as np
+import math
+
+omega = np.array([376.991118430775,
+502.654824574367,
+753.982236861550,
+1005.30964914873,
+1507.96447372310,
+2010.61929829747,
+3015.92894744620])
+
+err_NPW_4 = np.array([ 0.00216701147023616,
+0.00176295146124669,
+0.00130337855620765,
+0.000919437663223020,
+0.000673609558856852,
+0.000595467258613511,
+0.000440141889521627])
+
+
+err_NPW_6 = np.array([ 0.00149085703643568,
+0.00113989914906664,
+0.000769409511702538,
+0.000543214611743842,
+0.000463120362868336,
+0.000356876117029469,
+0.000265312921615620])
+
+
+err_NPW_8 = np.array([ 0.000993973425802366,
+0.000727275400853260,
+0.000505446079854768,
+0.000405825911503581,
+0.000302325437873150,
+0.000238027167967451,
+0.000198439997137332])
+
+
+
+import matplotlib.pyplot as plt
+
+golden = 1.61803398875
+width = 6
+height = width/golden
+
+
+
+fig = plt.figure(figsize=(width, height))
+
+
+p1, = plt.loglog(omega[:len(err_NPW_4)], err_NPW_4, label=r'NPW = 4',
+        	color='b', linewidth=2, linestyle='--', marker='o', markersize=8.0, zorder=2)
+
+p2, = plt.loglog(omega[:len(err_NPW_6)], err_NPW_6, label=r'NPW = 6', 
+			color='g', linewidth=2, linestyle= '--', marker='o', markersize=8.0, zorder=2) 
+
+p3, = plt.loglog(omega[:len(err_NPW_8)], err_NPW_8, label=r'NPW = 8', 
+			color='k', linewidth=2, linestyle= '--', marker='o', markersize=8.0, zorder=2)
+
+p4, = plt.loglog(omega, 0.75*err_NPW_4[0]/((omega/(omega[0]))**0.75), label=r'$\mathcal{O}(\omega^{-3/4})$', 
+			color='r', linewidth=2, linestyle= 'solid', markersize=8.0, zorder=2)
+
+# p1, = plt.loglog(omega, err_NPW_4, label=r'$\Vert u_{\mathbf{d}_{ex}} - u_{ex}\Vert_{L^2(\Omega)}$',
+#            color='g', linewidth=2, linestyle='--', marker='o', markersize=8.0, zorder=2)
+
+# p2, = plt.loglog(omega, err_NPW_6, label=r'$\mathcal{O}(\omega^{-1})$', color='r', linewidth=2, 
+# linestyle= '--', markersize=8.0, zorder=2)  # linestyle= 'solid'
+
+# plt.loglog(N_x**2, N_x**2 / 4.0e4, label=r' ', color='white', linewidth=0.0)
+first_legend = plt.legend(handles=[p1, p2, p3], loc=1, ncol=1, frameon=False, fontsize=15)
+ax = plt.gca().add_artist(first_legend)
+plt.legend(handles=[p4], loc=3, ncol=3, frameon=False, fontsize=18)
+
+# plt.legend(loc=0, ncol=1, frameon=False, fontsize=26)
+
+# plt.title('Exact Ray-FEM',fontsize=20)
+
+plt.xlabel(r'$\omega$', fontsize=18)
+plt.ylabel(r'Error', fontsize=18)
+
+plt.gca().tick_params(labelsize=14)
+
+plt.autoscale(True, 'both', True)
+plt.xlim(0.9*omega[0], 1.1*omega[-1])
+plt.ylim(0.8*err_NPW_8[-1], 1.2*err_NPW_4[0])
+plt.tight_layout(pad=0.5)
+
+fig.savefig('ex2_Lippmann_Schwinger.pdf')
+plt.show()
+
+plt.close('all')
+
+
+
