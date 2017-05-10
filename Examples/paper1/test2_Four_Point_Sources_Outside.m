71bd7b78e97be7ce3b0c88514f87208b57004b02
dinrker <junf2@uci.edu>
2017-05-09 23:42:11 -0800

    numerical examples in paper 1

---
+++ Examples/paper1/test2_Four_Point_Sources_Outside.m
@@ -0,0 +1,219 @@
+%% Convergence test for homogenenous case with exact ray information
+
+% add path
+clear;
+addpath(genpath('../../ifem/'));
+addpath('../Methods/');
+addpath('../NMLA/');
+addpath('../Helmholtz_data/');
+addpath('../Plots_Prints/');
+
+% set up
+pde = Helmholtz_data_four_point_sources;    % pde data
+xs = 20; ys = 20;       % source location
+Nray = 4;
+fquadorder = 9;    % the order of numerical quadrature
+solver = 'DIR';      % the type of solver for the resulting linear system
+plt = 0;                   % plot the solution or not
+sec_opt = 0;           % not using second order correction of NMLA
+
+test_num = 4;             % we test test_num examples
+NPW = 4;                   % number of points per wavelength
+
+% frequency
+high_omega = [40 80 120 160 240 320 400 480 640]*pi;
+low_omega = 2*sqrt(high_omega);
+
+% error
+low_l2_rayerr = 0*high_omega;      % L_2 ray error of low-freq waves
+high_l2_rayerr = 0*high_omega;     % L_2 ray error of high-freq waves
+
+rel_l2_err_numray = 0*high_omega;   % relative L_2 error of the numerical Ray-FEM solution
+rel_l2_err_exray = 0*high_omega;   % relative L_2 error of the exact Ray-FEM solution
+
+
+
+% wavelength
+high_wl = 2*pi./high_omega;
+low_wl = 2*pi./low_omega;
+
+% mesh size
+fh = 1./(NPW*round(high_omega/(2*pi)));      % fine mesh size
+ch = 1./(20*round(low_omega/(4*pi)));        % coarse mesh size
+
+
+%% Generate the domain sizes
+sd = 1/2;
+Rest = 20;          
+
+high_r = NMLA_radius(high_omega,Rest);
+md = sd + high_r;
+md = ceil(md*10)/10;      % middle domain size 
+
+low_r = NMLA_radius(low_omega,Rest);
+ld = md + low_r;
+ld = ceil(ld*10)/10;      % large domain size
+
+
+%% Tests
+tstart = tic;
+for ti = 1: test_num
+    omega = high_omega(ti);
+    h = fh(ti);  h_c = ch(ti);
+    fprintf(['-'*ones(1,80) '\n']);
+    fprintf('\nCase %d: \nomega/(2*pi) = %d,   1/h = %d   1/h_c = %d,  NPW = %d ',...
+        ti, round(omega/(2*pi)), 1/h, 1/h_c, NPW);
+    
+    
+    %% Step 1: Solve the Hemholtz equation with the same source but with a relative low frequency sqrt(\omega) by Standard FEM, mesh size \omega*h = constant
+    fprintf(['\n' '-'*ones(1,80) '\n']);
+    fprintf('Step1: S-FEM, low frequency \n');
+    tic;
+    omega = low_omega(ti);              % low frequency
+    a = ld(ti);                         % large domain 
+    [lnode,lelem] = squaremesh([-a,a,-a,a],h);
+    u_std = SFEM_IBC(lnode,lelem,omega,pde,fquadorder,solver,plt,xs,ys);
+    toc;
+    
+    
+    %% Step 2: Use NMLA to find ray directions d_c with low frequency sqrt(\omega)
+    fprintf([ '-'*ones(1,80) '\n']);
+    fprintf('Step2: NMLA, low frequency \n');
+    
+    % compute numerical derivatives 
+    [ux,uy] = num_derivative(u_std,h,2);
+    
+    a = md(ti);
+    [mnode,melem] = squaremesh([-a,a,-a,a],h);
+    
+    [cnode,celem] = squaremesh([-a,a,-a,a],h_c);
+    cN = size(cnode,1);    
+    cnumray_angle = zeros(cN,Nray);
+    
+    % NMLA
+    tic;
+    for i = 1:cN
+        x0 = cnode(i,1);  y0 = cnode(i,2);
+        r0 = sqrt((x0-xs)^2 + (y0-ys)^2);
+        c0 = pde.speed(cnode(i,:));
+        [cnumray_angle(i,:)] = NMLA(x0,y0,c0,omega,Rest,lnode,lelem,u_std,ux,uy,[],1/8,Nray,'num',sec_opt,plt);
+    end
+    cnumray = exp(1i*cnumray_angle);
+    numray1 = interpolation(cnode,celem,mnode,cnumray);
+    toc;
+    
+    % compute the ray errors
+    exray = pde.ray(mnode,xs,ys);
+    rayerr1 = numray1 - exray;
+    low_l2_rayerr(ti) = norm(rayerr1(:))*h/(norm(exray(:))*h);
+    
+    
+    %% Step 3: Solve the original Helmholtz equation by Ray-based FEM with ray directions d_c
+    fprintf(['-'*ones(1,80) '\n']);
+    fprintf('Step3: Ray-FEM, high frequency \n');
+   
+    tic;
+    omega = high_omega(ti);
+    ray = numray1;
+    uh1= RayFEM_IBC(mnode,melem,omega,pde,ray,fquadorder,plt,xs,ys);
+    toc;
+    
+    
+    
+    %% Step 4: NMLA to find original ray directions d_o with wavenumber k
+    fprintf(['-'*ones(1,80) '\n']);
+    fprintf('Step4: NMLA, high frequency \n');
+    
+    % compute numerical derivatives
+    [ux,uy] = num_derivative(uh1,h,2);
+    
+    a = sd;
+    [node,elem] = squaremesh([-a,a,-a,a],h);
+    
+    [cnode,celem] = squaremesh([-a,a,-a,a],h_c);
+    cN = size(cnode,1);
+    cnumray_angle = zeros(cN,Nray);
+    
+    
+    % NMLA
+    tic;
+    for i = 1:cN
+        x0 = cnode(i,1);  y0 = cnode(i,2);
+        r0 = sqrt((x0-xs)^2 + (y0-ys)^2);
+        c0 = pde.speed(cnode(i,:));
+        [cnumray_angle(i,:)] = NMLA(x0,y0,c0,omega,Rest,mnode,melem,uh1,ux,uy,[],1/8,Nray,'num',sec_opt,plt);
+    end
+    cnumray = exp(1i*cnumray_angle);
+    numray2 = interpolation(cnode,celem,node,cnumray);
+    toc;
+    
+    % compute the ray errors
+    exray = pde.ray(node,xs,ys);
+    rayerr2 = numray2 - exray;
+    high_l2_rayerr(ti) = norm(rayerr2(:))*h/(norm(exray(:))*h);    
+    
+    
+    %% Step 5: Solve the original Helmholtz equation by Ray-based FEM with ray directions d_o
+    fprintf(['-'*ones(1,80) '\n']);
+    fprintf('Step5: Ray-FEM, high frequency \n');
+    tic;
+    
+    % Ray-FEM solution
+    omega = high_omega(ti);
+    ray = numray2;
+    [uh2,~,~,~,rel_l2_err]= RayFEM_IBC(node,elem,omega,pde,ray,fquadorder,plt,xs,ys);
+    rel_l2_err_numray(ti) = rel_l2_err;
+    
+    %% Ray-FEM with exact ray
+    ray = pde.ray(node,xs,ys);
+    [uh,~,~,~,rel_l2_err]= RayFEM_IBC(node,elem,omega,pde,ray,fquadorder,plt,xs,ys);
+    rel_l2_err_exray(ti) = rel_l2_err;
+    toc;
+    
+end
+
+totaltime = toc(tstart);
+fprintf('\n\nTotal running time: % d minutes \n', totaltime/60);
+
+
+
+%% plots
+figure(11);
+subplot(1,2,1);
+show_convergence_rate(high_omega(1:test_num),low_l2_rayerr(1:test_num),'omega','low rel l2');
+subplot(1,2,2);
+show_convergence_rate(high_omega(1:test_num),high_l2_rayerr(1:test_num),'omega','high rel l2');
+
+figure(12);
+subplot(1,2,1);
+show_convergence_rate(high_omega(1:test_num),rel_l2_err_numray(1:test_num),'omega','NR-FEM rel l2');
+subplot(1,2,2);
+show_convergence_rate(high_omega(1:test_num),rel_l2_err_exray(1:test_num),'omega','ER-FEM rel l2');
+
+
+%% print results
+fprintf( ['\n' '-'*ones(1,80) '\n']);
+fprintf( 'omega:                   ');
+fprintf( '&  %.2e  ',high_omega );
+fprintf( '\nomega/2pi:               ');
+fprintf( '&  %.2e  ',high_omega/(2*pi) );
+fprintf( '\n\nGrid size h:             ');
+fprintf( '&  %.2e  ',fh);
+fprintf( '\n1/h:                     ');
+fprintf( '&  %.2e  ',1./fh);
+
+fprintf( ['\n' '-'*ones(1,80) '\n']);
+fprintf( '\nLow rel l2 ray error:    ');
+fprintf( '&  %1.2d  ',low_l2_rayerr);
+fprintf( '\n\nHigh rel l2 ray error:   ');
+fprintf( '&  %1.2d  ',high_l2_rayerr);
+
+fprintf( '\n\nNR-FEM rel l2 error:    ');
+fprintf( '&  %1.2d  ',rel_l2_err_numray);
+fprintf( '\n\nER-FEM rel l2 error:     ');
+fprintf( '&  %1.2d  ',rel_l2_err_exray);
+
+
+fprintf( ['\n' '-'*ones(1,80) '\n']);
+
+
---
+++ Examples/paper1/test4_Inside_Heterogeneous.m
@@ -0,0 +1,222 @@
+%% One point source inside heterogeneous medium domain:
+
+% add path
+clear;
+addpath(genpath('../../ifem/'));
+addpath('../Methods/');
+addpath('../NMLA/');
+addpath('../Helmholtz_data/');
+addpath('../Plots_Prints/');
+
+% set up
+xs = 1/10;   ys = 1/10;           % point source location
+speed = @(x) (3 - 2.5*exp( -((x(:,1)+1/8).^2 + (x(:,2)-0.1).^2)/0.8^2 )) ;    % medium speed
+cmin = 1/2;                      % minmum speed in the computational domain
+
+Nray = 1;
+fquadorder = 3;    % the order of numerical quadrature
+solver = 'DIR';      % the type of solver for the resulting linear system
+plt = 0;                   % plot the solution or not
+sec_opt = 0;           % not using second order correction of NMLA
+NPWs = 10;                   % number of points per wavelength
+test_num = 1;
+
+% frequency
+high_omega = 80*pi;
+low_omega = sqrt(high_omega);
+
+% wavelength
+high_wl = 2*pi*cmin./high_omega;
+low_wl = 2*pi*cmin./low_omega;
+
+% source neighborhood size
+r = 4*high_wl;
+
+% width of PML
+high_wpml = 4*high_wl(1)*ones(size(high_omega));
+ch = 1./(20*round(low_omega/(4*pi)));        % coarse mesh size
+low_wpml = ch.*ceil(low_wl(1)./ch);
+
+
+%% Generate the domain sizes
+sd = 1/2;
+Rest = 1;           % estimate of the distance to the source point
+
+high_r = NMLA_radius(high_omega,Rest);
+md = sd + high_r + high_wpml;
+md = ceil(md*10)/10;      % middle domain size
+
+% Rest = sqrt(2)*md;
+low_r = NMLA_radius(low_omega,Rest);
+ld = md + low_r + low_wpml;
+ld = ceil(ld*10)/10;      % large domain size
+
+
+%% Tests
+tstart = tic;
+for ti = 1: test_num
+    omega = high_omega;
+    NPW = NPWs(ti);
+    fh = 1./(NPW*round(high_omega/(2*pi*cmin)));      % fine mesh size
+    ch = 1./(20*round(low_omega/(4*pi*cmin)));        % coarse mesh size
+    
+    h = fh(ti);  h_c = ch(ti);
+    fprintf(['-'*ones(1,80) '\n']);
+    fprintf('\nCase %d: \nomega/(2*pi) = %d,   1/h = %d   1/h_c = %d,  NPW = %d ',...
+        ti, round(omega/(2*pi)), 1/h, 1/h_c, NPW);
+    
+    
+    %% Step 1: Solve the Hemholtz equation with the same source but with a relative low frequency sqrt(\omega) by Standard FEM, mesh size \omega*h = constant
+    fprintf(['\n' '-'*ones(1,80) '\n']);
+    fprintf('Step1: S-FEM, low frequency \n');
+    tic;
+    omega = low_omega(ti);              % low frequency
+    a = ld(ti);                         % large domain
+    wpml = low_wpml(ti);                % width of PML
+    sigmaMax = 25/wpml;                 % Maximun absorbtion
+    [lnode,lelem] = squaremesh([-a,a,-a,a],h);
+    A = assemble_Helmholtz_matrix_SFEM(lnode,lelem,omega,wpml,sigmaMax,speed,fquadorder);
+    b = assemble_RHS_SFEM(lnode,lelem, @(x)nodal_basis(xs,ys,h,x),fquadorder);
+    b = b/(h*h/2);
+    [~,~,isBdNode] = findboundary(lelem);
+    freeNode = find(~isBdNode);
+    lN = size(lnode,1);        u_std = zeros(lN,1);
+    u_std(freeNode) = A(freeNode,freeNode)\b(freeNode);
+    toc;
+    
+    
+    %% Step 2: Use NMLA to find ray directions d_c with low frequency sqrt(\omega)
+    fprintf([ '-'*ones(1,80) '\n']);
+    fprintf('Step2: NMLA, low frequency \n');
+    
+    % compute numerical derivatives
+    [ux,uy] = num_derivative(u_std,h,2);
+    
+    a = md(ti);
+    [mnode,melem] = squaremesh([-a,a,-a,a],h);
+    
+    [cnode,celem] = squaremesh([-a,a,-a,a],h_c);
+    cN = size(cnode,1);
+    cnumray = zeros(cN,Nray);
+    
+    % NMLA
+    tic;
+    for i = 1:cN
+        x0 = cnode(i,1);  y0 = cnode(i,2);
+        d0 = sqrt((x0-xs)^2 + (y0-ys)^2);
+        c0 = speed(cnode(i,:));
+        if d0 > r
+            Rest = d0;
+            [cnumray_angle] = NMLA(x0,y0,c0,omega,Rest,lnode,lelem,u_std,ux,uy,[],1/5,Nray,'num',sec_opt,plt);
+            cnumray(i) = exp(1i*cnumray_angle);
+        else
+            cnumray(i) = ex_ray([x0,y0],xs,ys,1);
+        end
+    end
+    
+    numray1 = interpolation(cnode,celem,mnode,cnumray);
+    toc;
+    
+    % compute the ray errors
+    exray = ex_ray(mnode,xs,ys,1);
+    xx = mnode(:,1) - xs;   yy = mnode(:,2) - ys;
+    rr = sqrt(xx.*xx + yy.*yy);
+    numray1(rr<=r) = exray(rr<=r);
+    
+    
+    %% Step 3: Solve the original Helmholtz equation by Ray-based FEM with ray directions d_c
+    fprintf(['-'*ones(1,80) '\n']);
+    fprintf('Step3: Ray-FEM, high frequency \n');
+    
+    tic;
+    omega = high_omega(ti);
+    wpml = high_wpml(ti);                % width of PML
+    sigmaMax = 25/wpml;                 % Maximun absorbtion
+    ray = numray1;
+    
+    % build linear system
+    A = assemble_Helmholtz_matrix_RayFEM(mnode,melem,omega,wpml,sigmaMax,speed,ray,fquadorder);
+    source = @(p) nodal_basis(xs,ys,h,p);
+    b = assemble_RHS_RayFEM(mnode,melem,omega,wpml,sigmaMax,source,speed,ray,fquadorder);
+    b = b/(h*h); %normalize b
+    
+    uh1 = RayFEM_direct_solver(mnode,melem,A,b,omega,ray,speed);
+    toc;
+    
+    
+    
+    %% Step 4: NMLA to find original ray directions d_o with wavenumber k
+    fprintf(['-'*ones(1,80) '\n']);
+    fprintf('Step4: NMLA, high frequency \n');
+    
+    % compute numerical derivatives
+    [ux,uy] = num_derivative(uh1,h,2);
+    
+    a = sd;
+    [node,elem] = squaremesh([-a,a,-a,a],h);
+    
+    [cnode,celem] = squaremesh([-a,a,-a,a],h_c);
+    cN = size(cnode,1);
+    cnumray = zeros(cN,Nray);
+    
+    
+    % NMLA
+    tic;
+    for i = 1:cN
+        x0 = cnode(i,1);  y0 = cnode(i,2);
+        d0 = sqrt((x0-xs)^2 + (y0-ys)^2);
+        c0 = speed(cnode(i,:));
+        if d0 > r
+            Rest = d0;
+            cnumray_angle = NMLA(x0,y0,c0,omega,Rest,mnode,melem,uh1,ux,uy,[],1/5,Nray,'num',sec_opt,plt);
+            cnumray(i) = exp(1i*cnumray_angle);
+        else
+            cnumray(i) = ex_ray([x0,y0],xs,ys,1);
+        end
+    end
+    numray2 = interpolation(cnode,celem,node,cnumray);
+    toc;
+    
+    % compute the ray errors
+    exray = ex_ray(node,xs,ys,1);
+    xx = node(:,1) - xs;   yy = node(:,2) - ys;
+    rr = sqrt(xx.*xx + yy.*yy);
+    numray2(rr<=r) = exray(rr<=r);
+    
+    
+    %% Step 5: Solve the original Helmholtz equation by Ray-based FEM with ray directions d_o
+    fprintf(['-'*ones(1,80) '\n']);
+    fprintf('Step5: Ray-FEM, high frequency \n');
+    
+    tic;
+    omega = high_omega(ti);
+    wpml = high_wpml(ti);                % width of PML
+    sigmaMax = 25/wpml;                 % Maximun absorbtion
+    ray = numray2;
+    
+    % build linear system
+    A = assemble_Helmholtz_matrix_RayFEM(node,elem,omega,wpml,sigmaMax,speed,ray,fquadorder);
+    source = @(p) nodal_basis(xs,ys,h,p);
+    b = assemble_RHS_RayFEM(node,elem,omega,wpml,sigmaMax,source,speed,ray,fquadorder);
+    b = b/(h*h); %normalize b
+
+    uh = RayFEM_direct_solver(node,elem,A,b,omega,ray,speed);   
+    toc;
+    
+    
+end
+
+totaltime = toc(tstart);
+fprintf('\n\nTotal running time: % d minutes \n', totaltime/60);
+
+
+%% map to polar
+figure(40);
+subplot(1,2,1);
+ray_field(ray,node,20,1/10);
+subplot(1,2,2);
+showsolution(node,elem,real(uh),2);
+colorbar;
+
+
+
