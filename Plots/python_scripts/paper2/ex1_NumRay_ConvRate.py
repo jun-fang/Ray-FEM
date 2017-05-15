cc6f6de6bb7892e7347bedaab04593c00e351b15
dinrker <junf2@uci.edu>
2017-05-14 22:22:04 -0800

    add plots

---
+++ Plots/python_scripts/paper2/ex1_NumRay_ConvRate.py
@@ -0,0 +1,97 @@
+import numpy as np
+import math
+
+omega = np.array([376.991118430775,
+502.654824574367,
+753.982236861550,
+1005.30964914873,
+1507.96447372310,
+2010.61929829747,
+3015.92894744620])
+
+omega = omega/np.pi
+
+err_NPW_4 = np.array([0.00270157770281540,
+0.00219734201275802,
+0.00170110076048316,
+0.00141722055515402,
+0.00116206600585611,
+0.000834283365116496,
+0.000760021129535664])
+
+
+err_NPW_6 = np.array([ 0.00155179897461391,
+0.00117260261040538,
+0.000956857981728328,
+0.000729819228791556,
+0.000586008771195049,
+0.000542605986265505,
+0.000427214395423089])
+
+
+err_NPW_8 = np.array([ 0.00103175668543789,
+0.000777913307163148,
+0.000617847788595371,
+0.000492830804433761,
+0.000408654829864075,
+0.000354809212835210,
+0.000264580813005265])
+
+
+
+import matplotlib.pyplot as plt
+
+golden = 1.61803398875
+width = 6
+height = width/golden
+
+
+
+fig = plt.figure(figsize=(width, height))
+
+
+p1, = plt.loglog(omega[:len(err_NPW_4)], err_NPW_4, label=r'NPW = 4',
+        	color='b', linewidth=2, linestyle='--', marker='o', markersize=8.0, zorder=2)
+
+p2, = plt.loglog(omega[:len(err_NPW_6)], err_NPW_6, label=r'NPW = 6', 
+			color='g', linewidth=2, linestyle= '--', marker='o', markersize=8.0, zorder=2) 
+
+p3, = plt.loglog(omega[:len(err_NPW_8)], err_NPW_8, label=r'NPW = 8', 
+			color='k', linewidth=2, linestyle= '--', marker='o', markersize=8.0, zorder=2)
+
+p4, = plt.loglog(omega[:len(err_NPW_4)], 0.7*err_NPW_4[0]/((omega[:len(err_NPW_4)]/(omega[0]))**0.6), label=r'$\mathcal{O}(\omega^{-0.6})$', 
+			color='r', linewidth=2, linestyle= 'solid', markersize=8.0, zorder=2)
+
+# p1, = plt.loglog(omega, err_NPW_4, label=r'$\Vert u_{\mathbf{d}_{ex}} - u_{ex}\Vert_{L^2(\Omega)}$',
+#            color='g', linewidth=2, linestyle='--', marker='o', markersize=8.0, zorder=2)
+
+# p2, = plt.loglog(omega, err_NPW_6, label=r'$\mathcal{O}(\omega^{-1})$', color='r', linewidth=2, 
+# linestyle= '--', markersize=8.0, zorder=2)  # linestyle= 'solid'
+
+# plt.loglog(N_x**2, N_x**2 / 4.0e4, label=r' ', color='white', linewidth=0.0)
+first_legend = plt.legend(handles=[p1, p2, p3], loc=1, ncol=1, frameon=False, fontsize=15)
+ax = plt.gca().add_artist(first_legend)
+plt.legend(handles=[p4], loc=3, ncol=3, frameon=False, fontsize=18)
+
+# plt.legend(loc=0, ncol=1, frameon=False, fontsize=26)
+
+# plt.title('Exact Ray-FEM',fontsize=20)
+
+plt.xlabel(r'$\omega/\pi$', fontsize=18)
+plt.ylabel(r'Rel $L^2$ Err', fontsize=18)
+
+plt.gca().tick_params(labelsize=14)
+
+plt.autoscale(True, 'both', True)
+# plt.xlim(0.9*omega[0], 1.1*omega[len(err_NPW_4)-1])
+plt.xlim(100, 1100)
+plt.ylim(0.8*err_NPW_8[-1], 1.2*err_NPW_4[0])
+plt.tight_layout(pad=0.5)
+
+fig.savefig('ex1_NumRay_ConvRate.pdf')
+plt.show()
+
+plt.close('all')
+
+
+
--- Plots/python_scripts/paper2/ex3_Constant_Gradient_Velocity.py
+++ Plots/python_scripts/paper2/ex3_Constant_Gradient_Velocity.py
@@ -3,30 +3,31 @@ import math
 
 omega = np.array([376.991118430775,
 502.654824574367,
-628.318530717959,
 753.982236861550,
 1005.30964914873,
-1507.96447372310])
+1256.63706143592,
+1570.79632679490,
+1884.95559215388])
 
-err_NPW_4 = np.array([ 0.00593333820896539,
-0.00504970453794580,
-0.00443973612226285,
-0.00389485672038733,
-0.00295946498934470,
-0.00265063345600905])
-
-
-err_NPW_6 = np.array([ 0.00318351765909728,
-0.00276862164592117,
-0.00266672917969444,
-0.00217927113462856,
-0.00179874751899359])
+err_NPW_4 = np.array([ 0.00258930982124353,
+0.00215155912258392,
+0.00159809319661259,
+0.00132432620681086,
+0.00120835699929618,
+0.000922924362604902])
 
 
-err_NPW_8 = np.array([ 0.00210843849876511,
-0.00182437498860183,
-0.00154580456188329,
-0.00134710099116785])
+err_NPW_6 = np.array([ 0.00146232132508687,
+0.00130361456590141,
+0.000879405066841832,
+0.000717086709329996,
+0.000646749254357502])
+
+
+err_NPW_8 = np.array([ 0.000975879449384578,
+0.000861420339184053,
+0.000626648187115122,
+0.000543591774964586])
 
 
 
@@ -50,7 +51,7 @@ p2, = plt.loglog(omega[:len(err_NPW_6)], err_NPW_6, label=r'NPW = 6',
 p3, = plt.loglog(omega[:len(err_NPW_8)], err_NPW_8, label=r'NPW = 8', 
 			color='k', linewidth=2, linestyle= '--', marker='o', markersize=8.0, zorder=2)
 
-p4, = plt.loglog(omega, 0.7*err_NPW_4[0]/((omega/(omega[0]))**0.5), label=r'$\mathcal{O}(\omega^{-1/2})$', 
+p4, = plt.loglog(omega[:len(err_NPW_4)], 0.7*err_NPW_4[0]/((omega[:len(err_NPW_4)]/(omega[0]))**0.5), label=r'$\mathcal{O}(\omega^{-1/2})$', 
 			color='r', linewidth=2, linestyle= 'solid', markersize=8.0, zorder=2)
 
 # p1, = plt.loglog(omega, err_NPW_4, label=r'$\Vert u_{\mathbf{d}_{ex}} - u_{ex}\Vert_{L^2(\Omega)}$',
@@ -69,7 +70,7 @@ plt.legend(handles=[p4], loc=3, ncol=3, frameon=False, fontsize=18)
 # plt.title('Exact Ray-FEM',fontsize=20)
 
 plt.xlabel(r'$\omega$', fontsize=18)
-plt.ylabel(r'Error', fontsize=18)
+plt.ylabel(r'Rel $L^2$ Err', fontsize=18)
 
 plt.gca().tick_params(labelsize=14)
 
@@ -78,7 +79,7 @@ plt.xlim(0.9*omega[0], 1.1*omega[-1])
 plt.ylim(0.8*err_NPW_8[-1], 1.2*err_NPW_4[0])
 plt.tight_layout(pad=0.5)
 
-fig.savefig('ex1_NumRay_ConvRate.pdf')
+fig.savefig('ex3_Constant_Gradient_Velocity.pdf')
 plt.show()
 
 plt.close('all')
